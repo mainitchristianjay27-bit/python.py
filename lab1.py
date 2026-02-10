a11a2792e4d4da18d019fec60f2b1014dc84d1fa
Eng. CJ <mainit.chrisianjay27@gmail.com>
2026-02-10 17:34:52 +0800

    #First Commit

---
+++ lab1.py
@@ -0,0 +1,20 @@
+name = "Christian Jay Mainit"
+age = 18
+Height = 156
+Currently_Working = False
+
+print("Fullname:" , name)
+print(type(name))
+print ()
+
+print("Age:" , age)
+print(type(age))
+print()
+
+print("Height_in_centimeters:" , Height)
+print(type(Height))
+print()
+
+print("Currently_Working?:" , Currently_Working)
+print(type(Currently_Working))
+print()

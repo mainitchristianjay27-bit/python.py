a0cc3bfb2b7c9fd82eeeaec2b2ff3e4a0ede0dd0
Engr. CJ <mainitchristian27@gmail.com>
2026-02-13 10:25:10 +0800

    Add basicCalculator.py

--- basicCalculator.py
+++ basicCalculator.py
@@ -1,3 +1,4 @@
+result = ""
 calculator = float(input("Enter your first number: "))
 calculator2 = float(input("Enter your second number: "))
 
@@ -9,18 +10,21 @@ print("4. Subtraction.")
 choice = input("Enter your choice: ")
 
 if choice == "1":
-    print("Result:", calculator * calculator2)
+    result = calculator * calculator2
+    print("Result", int(result))
 elif choice == "2":
     if calculator2 != 0:
         print("Result", calculator / calculator2)
     else: 
         print("Error: Division by zero is not allowed.")
 elif choice == "3":
-    print("Result:", calculator + calculator2)
+    result = calculator + calculator2
+    print("Result:" , int(result))
 elif choice == "4":
-    print("Result:", calculator - calculator2)
+    result = calculator - calculator2
+    print("Result:", int(result))
 else:
-    print("Error. Please enter the correct number!")            
+    print("Error! Not in the choices.")            
 
 
 
